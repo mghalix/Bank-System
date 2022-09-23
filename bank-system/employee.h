e00465db4764a9464c8b6fd7ce2ff26517e17f16
Mohanad <321213360@sha.edu.eg>
2022-09-23 11:21:35 +0200

    added convention rules

--- bank-system/employee.h
+++ bank-system/employee.h
@@ -4,43 +4,43 @@
 #include "validation.h"
 class Employee : public Person {
 private:
-    //Attributes
-    float salary;
-    double balance;
+  //Attributes
+  float salary;
+  double balance;
 
 public:
-    //Constructors 
-    Employee() : Person() {
-        salary = 0.0;
-        balance = 0.0;
-    }
+  //Constructors 
+  Employee() : Person() {
+    salary = 0.0;
+    balance = 0.0;
+  }
 
-    Employee(int id, std::string name, std::string password, double balance, float salary) : Person(id, name, password) {
-        this->balance = balance;
-        Validation::checkSalary(salary);
-        this->salary = salary;
-    }
+  Employee(int id, std::string name, std::string password, double balance, float salary) : Person(id, name, password) {
+    this->balance = balance;
+    Validation::checkSalary(salary);
+    this->salary = salary;
+  }
 
-    //Setters
-    void setSalary(float salary) {
-        Validation::checkSalary(salary);
-        this->salary = salary;
-    }
+  //Setters
+  void setSalary(float salary) {
+    Validation::checkSalary(salary);
+    this->salary = salary;
+  }
 
-    void setBalance(double balance) {
-        this->balance = balance;
-    }
+  void setBalance(double balance) {
+    this->balance = balance;
+  }
 
-    //Getters
-    float getSalary() const { return salary; }
+  //Getters
+  float getSalary() const { return salary; }
 
-    double getBalance() const { return balance; }
+  double getBalance() const { return balance; }
 
-    //Methods
-    void display() override {
-        std::cout << "Name: " << getName() << std::endl
-            //<< "password: " << getPassword() << std::endl
-            << "ID: #" << getID() << std::endl
-            << "Salary: $" << salary << std::endl;
-    }
+  //Methods
+  void display() override {
+    std::cout << "Name: " << getName() << std::endl
+      //<< "password: " << getPassword() << std::endl
+      << "ID: #" << getID() << std::endl
+      << "Salary: $" << salary << std::endl;
+  }
 };
--- bank-system/person.h
+++ bank-system/person.h
@@ -41,5 +41,4 @@ public:
 		Validation::checkPassword(password);
 		this->password = password;
 	}
-
 };
