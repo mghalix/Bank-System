e92ec5ed14e9a2e15101e3c415529036a0b5aef8
Mohanad <321213360@sha.edu.eg>
2022-09-25 00:21:23 +0200

    organized project

--- bank-system/data_source_interface.h
+++ bank-system/data_source_interface.h
@@ -1,14 +1,16 @@
 #pragma once
 #include "client.h"
 #include <vector>
+#include "admin.h"
+#include "employee.h"
 class Data {
 	virtual void addClient(Client) = 0;
-	// virtual void addEmployee(Employee) = 0;
-	// virtual void addAdmin(Admin) = 0;
+	virtual void addEmployee(Employee) = 0;
+	virtual void addAdmin(Admin) = 0;
 
-	// virtual vector<Client> getAllClients() = 0;
-	// virtual vector<Employee> getAllEmployees() = 0;
-	// virtual vector<Employee> getAllAdmins() = 0;
+	virtual vector<Client> getAllClients() = 0;
+	virtual vector<Employee> getAllEmployees() = 0;
+	virtual vector<Admin> getAllAdmins() = 0;
 
 	virtual void removeAllClients() = 0;
 	virtual void removeAllEmployees() = 0;
--- bank-system/file_manager.h
+++ bank-system/file_manager.h
@@ -1,38 +1,63 @@
 #pragma once
 #include "data_source_interface.h"
 class FileManager : Data {
-	void addClient(Client cli) override {
+  // Metwally's Task
+  void addClient(Client cli) override {
 
-	}
-	// void addEmployee(Employee emp) override {
+  }
+  void addEmployee(Employee emp) override {
 
-	// }
+  }
 
-	// void addAdmin(Admin adm) override {
+  void addAdmin(Admin adm) override {
 
-	// }
+  }
 
-	// vector<Client> getAllClients() override {
 
-	// }
 
-	// vector<Employee> getAllEmployees() override {
+  // Ghali's Task
+  vector<Client> getAllClients() override {
+    vector<Client> clients;
 
-	// }
 
-	// vector<Admin> getAllAdmins() override {
 
-	// }
 
-	void removeAllClients() override {
 
-	}
 
-	void removeAllEmployees() override {
+    return clients;
+  }
 
-	}
+  vector<Employee> getAllEmployees() override {
+    vector<Employee> employees;
 
-	void removeAllAdmins() override {
 
-	}
+
+
+
+
+    return employees;
+  }
+
+  vector<Admin> getAllAdmins() override {
+    vector<Admin> admins;
+
+
+
+
+
+
+    return admins;
+  }
+
+  void removeAllClients() override {
+
+  }
+
+  void removeAllEmployees() override {
+
+  }
+
+  void removeAllAdmins() override {
+
+  }
 };
