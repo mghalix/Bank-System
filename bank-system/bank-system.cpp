01c178220569378a9ff209f96e20b9c47bdb3b0e
Mohanad <321213360@sha.edu.eg>
2022-09-25 15:27:59 +0200

    finished my task

--- bank-system/bank-system.cpp
+++ bank-system/bank-system.cpp
@@ -4,23 +4,34 @@
 #include "employee.h"
 using namespace std;
 int main() {
-    Client c1(1500);
-    try {
-        c1.setID(123);
-        c1.setName("Ahmed");
-        c1.setPassword("15678");
-    }
-    catch (const char *msg) {
-        cout << msg << endl;
-    }
-    //Outputs no_password here because the program didn't accept the password setter above in the try block above, thus used the default password value initialized inside the Client's class constructor
-    cout << c1.getPassword() << endl;
-    //Updating to an acceptable password(length 8-20)
-    cout << "-----\n";
-    c1.setPassword("12345678");
-    c1.display();
-    Employee emp(1234, "Mohanad", "13213123", 56000);
+  Client c1(1500);
+  try {
+    c1.setID(123);
+    c1.setName("Ahmed");
+    c1.setPassword("15678");
+  }
+  catch (const char *msg) {
+    cout << msg << endl;
+  }
+  //Outputs no_password here because the program didn't accept the password setter above in the try block above, thus used the default password value initialized inside the Client's class constructor
+  cout << c1.getPassword() << endl;
+  //Updating to an acceptable password(length 8-20)
+  cout << "-----\n";
+  c1.setPassword("12345678");
+  // c1.display();
+  Employee emp(1234, "Mohanad", "13213123", 56000);
 
-    emp.display();
-    return 0;
+  // emp.display();
+  FileManager fm;
+  fm.addEmployee(emp);
+  try {
+
+    fm.addEmployee(Employee(123, "ahmed", "12312312", 7000.54));
+  }
+  catch (const char *msg) {
+    cerr << msg << endl;
+  }
+  vector<Employee> emps = fm.getAllEmployees();
+  cout << emps[1].getSalary() << endl;
+  return 0;
 }
--- bank-system/file_manager.h
+++ bank-system/file_manager.h
@@ -3,78 +3,96 @@
 #include <fstream>
 #include <string>
 class FileManager : Data {
+public:
   // Metwally's Task
   void addClient(Client cli) override {
-      fstream ClientInfo;
-      ClientInfo.open("Client.txt", ios::app);
-      string line;
-      line = to_string(cli.getID()) + '|' + cli.getName() + '|' + cli.getPassword() + '|' + to_string(cli.getBalance());
-      ClientInfo << line << endl;
-      ClientInfo.close();
+    fstream ClientInfo;
+    ClientInfo.open("Clients.txt", ios::app);
+    string line;
+    line = to_string(cli.getID()) + '|' + cli.getName() + '|' + cli.getPassword() + '|' + to_string(cli.getBalance());
+    ClientInfo << line << endl;
+    ClientInfo.close();
   }
+
   void addEmployee(Employee emp) override {
-      fstream ClientInfo;
-      ClientInfo.open("Employee.txt", ios::app);
-      string line;
-      line = to_string(emp.getID()) + '|' + emp.getName() + '|' + emp.getPassword() + '|' + to_string(emp.getSalary());
-      ClientInfo << line << endl;
-      ClientInfo.close();
+    fstream ClientInfo;
+    ClientInfo.open("Employee.txt", ios::app);
+    string line;
+    line = to_string(emp.getID()) + '|' + emp.getName() + '|' + emp.getPassword() + '|' + to_string(emp.getSalary());
+    ClientInfo << line << endl;
+    ClientInfo.close();
   }
 
   void addAdmin(Admin adm) override {
-      fstream ClientInfo;
-      ClientInfo.open("Admin.txt", ios::app);
-      string line;
-      line = to_string(adm.getID()) + '|' + adm.getName() + '|' + adm.getPassword() + '|' + to_string(adm.getSalary());
-      ClientInfo << line << endl;
-      ClientInfo.close();
+    fstream ClientInfo;
+    ClientInfo.open("Admin.txt", ios::app);
+    string line;
+    line = to_string(adm.getID()) + '|' + adm.getName() + '|' + adm.getPassword() + '|' + to_string(adm.getSalary());
+    ClientInfo << line << endl;
+    ClientInfo.close();
   }
-
-
 
   // Ghali's Task
   vector<Client> getAllClients() override {
     vector<Client> clients;
-
-
-
-
-
-
+    ifstream fin("Clients.txt");
+    fin.seekg(0, ios::beg);
+    while (fin.peek() != EOF) {
+      string arr[4];
+      getline(fin, arr[0], '|');
+      getline(fin, arr[1], '|');
+      getline(fin, arr[2], '|');
+      getline(fin, arr[3], '\n');
+      clients.push_back(Client(stoi(arr[0]), arr[1], arr[2], stod(arr[3])));
+    }
+    fin.close();
     return clients;
   }
 
   vector<Employee> getAllEmployees() override {
     vector<Employee> employees;
-
-
-
-
-
-
+    ifstream fin("Employee.txt");
+    fin.seekg(0, ios::beg);
+    while (fin.peek() != EOF) {
+      string arr[4];
+      getline(fin, arr[0], '|');
+      getline(fin, arr[1], '|');
+      getline(fin, arr[2], '|');
+      getline(fin, arr[3], '\n');
+      employees.push_back(Employee(stoi(arr[0]), arr[1], arr[2], stod(arr[3])));
+    }
+    fin.close();
     return employees;
   }
 
   vector<Admin> getAllAdmins() override {
     vector<Admin> admins;
-
-
-
-
-
-
+    ifstream fin("Admin.txt");
+    fin.seekg(0, ios::beg);
+    while (fin.peek() != EOF) {
+      string arr[4];
+      getline(fin, arr[0], '|');
+      getline(fin, arr[1], '|');
+      getline(fin, arr[2], '|');
+      getline(fin, arr[3], '\n');
+      admins.push_back(Admin(stoi(arr[0]), arr[1], arr[2], stof(arr[3])));
+    }
+    fin.close();
     return admins;
   }
 
   void removeAllClients() override {
-
+    ofstream ofs("Clients.txt", ios::trunc);
+    ofs.close();
   }
 
   void removeAllEmployees() override {
-
+    ofstream ofs("Employee.txt", ios::trunc);
+    ofs.close();
   }
 
   void removeAllAdmins() override {
-
+    ofstream ofs("Admin.txt", ios::trunc);
+    ofs.close();
   }
 };
