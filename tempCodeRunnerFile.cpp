254cb6978bcd49220ef1e3f69b374cec11a3c1b1
Mohanad Ghali <74559094+mghalix@users.noreply.github.com>
2022-10-31 07:15:55 +0200

    Delete tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
- cli1("Hassan", "nourisgay", 12314.413);
-  Client cli2("Hussein", "923812344", 123123);
-  fm.addClient(cli1);
-  fm.addClient(cli2);
-  Admin adm1("Ahmed", "nourisgay", 9000.905);
-  Admin adm2("Assem", "12342412412", 11000.970);
-  fm.addAdmin(adm1);
-  fm.add
